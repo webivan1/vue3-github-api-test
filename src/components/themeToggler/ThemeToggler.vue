24aaf9e53ab6216afb5f7a10403222b1a7329eb8
Ivan Maltsev <ivan.maltsev.php@gmail.com>
2024-02-10 00:18:20 +0000

    Inital commit, added shadcn ui, tailwindcss, data theme toggler

---
+++ src/components/themeToggler/ThemeToggler.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faSun, faMoon } from '@fortawesome/free-regular-svg-icons'
+import { useTheme } from '@/components/themeToggler/useTheme'
+import { ThemeEnum } from '@/components/themeToggler/enums/ThemeEnum'
+
+const { theme, changeTheme } = useTheme()
+</script>
+
+<template>
+  <Button
+    v-if="theme === ThemeEnum.DARK"
+    variant="outline"
+    size="icon"
+    @click="changeTheme(ThemeEnum.LIGHT)"
+  >
+    <FontAwesomeIcon :icon="faSun" />
+  </Button>
+  <Button v-else variant="outline" size="icon" @click="changeTheme(ThemeEnum.DARK)">
+    <FontAwesomeIcon :icon="faMoon" />
+  </Button>
+</template>
