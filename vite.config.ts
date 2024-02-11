f400aa7e9e403f8c752c1d0504a72f00f27dcb11
Ivan Maltsev <ivan.maltsev.php@gmail.com>
2024-02-11 02:35:21 +0000

    test gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,21 +7,17 @@ import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-
-  return {
-    base: process.env.VITE_BASE_URL,
-    plugins: [vue()],
-    css: {
-      postcss: {
-        plugins: [tailwind(), autoprefixer()]
-      }
-    },
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
+export default defineConfig({
+  base: process.env.VITE_BASE_URL ?? '/',
+  plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
